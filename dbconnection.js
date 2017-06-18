a76aa2b112c739c1e6d1e1e34cdf26422af558a8
bender <felipecbender@gmail.com>
2017-06-18 19:12:04 -0300

    Ajuste configuração de banco.

--- dbconnection.js
+++ dbconnection.js
@@ -2,6 +2,8 @@ var mysql = require('mysql');
 var connection = mysql.createPool({
     host: 'us-cdbr-iron-east-03.cleardb.net',
     user: 'b3aa86c85768e5',
-    password: '2e5f247b'
+    password: '2e5f247b',
+    database: 'heroku_2403e0367b0ac6e'
 });
+
 module.exports = connection;
